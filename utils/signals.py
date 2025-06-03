d188ea16cb36a75e47162c6236f19fd246415380
YeabsiraNigusse <nigusseyeabsira@gmail.com>
2025-06-03 12:14:51 +0300

    chore: deleted unessasary files

--- utils/signals.py
+++
@@ -1,8 +0,0 @@
-def generate_signals(df):
-    """
-    Create basic Buy/Sell signals using SMA crossovers.
-    """
-    df['Signal'] = 0
-    df.loc[df['SMA_20'] > df['SMA_50'], 'Signal'] = 1
-    df.loc[df['SMA_20'] < df['SMA_50'], 'Signal'] = -1
-    return df
